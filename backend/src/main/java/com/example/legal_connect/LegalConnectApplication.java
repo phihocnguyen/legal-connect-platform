d1ed113a85b36509114d6f804cb206c27af94f29
krisng <ng.phihoc123@gmail.com>
2025-09-19 17:41:16 +0700

    feat: initialize backend

---
+++ backend/src/main/java/com/example/legal_connect/LegalConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.legal_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalConnectApplication.class, args);
+	}
+
+}
